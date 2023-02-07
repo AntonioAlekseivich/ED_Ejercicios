073b12907a85becb7bcfbe4da2bf4ef465d7137c
anton <antonbgi98@gmail.com>
2023-02-07 20:10:45 +0100

    actualizado test persona

--- ED_Ejercicios/src/unidad02_PruebasDeSoftware/junit_Ejercicio/TestPersona.java
+++ ED_Ejercicios/src/unidad02_PruebasDeSoftware/junit_Ejercicio/TestPersona.java
@@ -4,15 +4,10 @@ package junit_Ejercicio;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import Persona;
-import Persona;
-import Persona;
 
 class TestPersona {
 
 	@Test
-	
-	
 	void cumplirAnio() {
 		Persona española = new Persona();
 		española.cumplirAnio();
